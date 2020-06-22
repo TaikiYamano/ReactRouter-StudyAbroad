f43f4797a8a2e4a4ba3b28ac3133837d815bf6ff
山野大樹 <taikiyamano@yamanodaikinoMacBook-ea.local>
2020-06-22 22:07:09 +0900

    ヘッダー作成

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Link,
+} from 'react-router-dom';
+import '../../App.css';
+
+function Header(){
+    return(
+        <header className="header_wrapper">
+            <nav>
+                <BrowserRouter>
+                    <ul className="header_menu">
+                        <li><Link to="/">Top</Link></li>
+                        <li><Link to="/study">Study</Link></li>
+                        <li><Link to="/trip">Trip</Link></li>
+                        <li><Link to="/dance">Dance</Link></li>
+                    </ul>
+                </BrowserRouter>
+            </nav>
+            <div className="header_title_img">
+                <h1 className="header_title">My Life In The U.S</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/Header/Header';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
