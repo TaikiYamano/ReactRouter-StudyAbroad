450da613a36a12b76ce5b2137aee71c6cbb04c0d
山野大樹 <taikiyamano@yamanodaikinoMacBook-ea.local>
2020-06-24 07:05:16 +0900

    Topコンポーネント実装

---
+++ src/components/Top/Top.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../../App.css';
+
+function Top(){
+    return(
+        <div class="whole_wrapper">
+            <div class="inner">
+                <h1 className="page_title">Top</h1>
+                <div className="content">
+                    <div className="img_space img_left">
+
+                    </div>
+                    <p className="text_space text_right">
+                        When I was a freshman in a collage, I studied abroad in America for 9 months(from September,2016 to 
+                        May,2017) as my collage program.<br/>
+                        While I studied in America, I experienced a lot of 
+                        things, and learned a lot of things, languages, different culture, different life styles, and so on.
+                    </p>
+                </div>
+                <div className="content">
+                    <p className="text_space text_left">
+                        I think the time I spend there is one of the best memory in my life, also, I think this experience 
+                        inspired me to study programmings and be a web engineer.<br/>
+                        So, in this web page, I would like to share  my memory in America with you!<br/>
+                        This web page is created by React and React Router.
+                    </p>
+                    <div className="img_space img_right">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Top;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './components/Header/Header';
+import Top from './components/Top/Top';
+import {
+  BrowserRouter,
+  Route,
+} from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    
+    <BrowserRouter>
+      <Header />
+      <Route path="/" exact component={Top} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
